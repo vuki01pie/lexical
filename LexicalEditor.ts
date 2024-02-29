02f10a4e512bc6bf0fa8eab725c72659ef3c9acb
vuki01pie <mateovukovic01pie@gmail.com>
2024-02-29 17:18:39 +0100

    Introduce LexicalUpdates and theme config

--- LexicalEditor.ts
+++ LexicalEditor.ts
@@ -1,6 +1,19 @@
-export type CreateEditorArgs = {};
+import { internalGetActiveEditor } from "./LexicalUpdates";
+
+export type EditorThemeClasses = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [key: string]: any;
+};
+
+export type CreateEditorArgs = {
+  theme?: EditorThemeClasses;
+};
 
 export function createEditor(editorConfig?: CreateEditorArgs): LexicalEditor {
+  const config = editorConfig || {};
+  const activeEditor = internalGetActiveEditor();
+  const theme = config.theme || {};
+
   return new LexicalEditor();
 }
 
